29e4a16b9b9fbc0c1e85985f8ebce2578e62a193
samsonajulor <samsonajulor@gmail.com>
2024-07-28 13:07:15 +0100

    updated

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 import { fileURLToPath, URL } from 'url';
+import * as path from 'path'; // Import path from Node.js
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import environment from 'vite-plugin-environment';
@@ -14,34 +15,32 @@ export default defineConfig({
   optimizeDeps: {
     esbuildOptions: {
       define: {
-        global: "globalThis",
+        global: 'globalThis',
       },
     },
   },
   server: {
     proxy: {
-      "/api": {
-        target: "http://127.0.0.1:4943",
+      '/api': {
+        target: 'http://127.0.0.1:4943',
         changeOrigin: true,
       },
     },
   },
-  plugins: [
-    react(),
-    environment("all", { prefix: "CANISTER_" }),
-    environment("all", { prefix: "DFX_" }),
-  ],
+  plugins: [react(), environment('all', { prefix: 'CANISTER_' }), environment('all', { prefix: 'DFX_' })],
   test: {
     environment: 'jsdom',
-    setupFiles: 'src/setupTests.js',
+    setupFiles: 'src/setupTests.js', // Ensure the path is correct
   },
   resolve: {
     alias: [
       {
-        find: "declarations",
-        replacement: fileURLToPath(
-          new URL("../declarations", import.meta.url)
-        ),
+        find: 'declarations',
+        replacement: fileURLToPath(new URL('../declarations', import.meta.url)),
+      },
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src'),
       },
     ],
   },
