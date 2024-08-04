f83b49075fec0b4c275c1fd190a79e31c1a34e34
chizobavictory <chizoba.victory1@gmail.com>
2024-08-04 22:13:03 +0100

    refactor: Remove unused SVG files and update image imports

--- src/web/src/components/blocks/AuthImage.tsx
+++ src/web/src/components/blocks/AuthImage.tsx
@@ -1,3 +1,6 @@
+import avatar from '../../images/Ellipse.png';
+import dbIcon from '../../images/Vector.svg';
+
 export function AuthImage() {
   return (
     <div className='hidden lg:flex lg:justify-center lg:items-center bg-customBlue mt-4 ml-4 mb-4 relative'>
@@ -7,12 +10,12 @@ export function AuthImage() {
       <div className='flex flex-col justify-center items-center'>
         <div className='text-center text-white text-xl font-normal font-nunito'>@quickDB is the fastest and best db out there</div>
         <div className='flex gap-2 mt-2 items-center'>
-          <img src='/images/ellipse.png' alt='avatar' className='w-8 h-8 rounded-full' />
+          <img src={avatar} alt='avatar' className='w-8 h-8 rounded-full' />
           <p className='text-white text-xl font-normal font-nunito'>@nobeijoan</p>
         </div>
       </div>
-      <div className='absolute bottom-0 left-0 p-8'>
-        <img src='/path/to/db-icon.svg' alt='database icon' className='w-20 h-20' />
+      <div className='absolute bottom-0 left-0 '>
+        <img src={dbIcon} alt='database icon' className='w-22 h-22' />
       </div>
     </div>
   );
