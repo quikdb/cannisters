c5c67f710f249dc67d88aa61673082990894ae73
chizobavictory <chizoba.victory1@gmail.com>
2024-08-28 10:48:27 +0100

    refactor: Update SideBar component to fix commented out links

--- src/web/src/components/blocks/SideBar.tsx
+++ src/web/src/components/blocks/SideBar.tsx
@@ -42,7 +42,7 @@ export function SideBar() {
               Projects
               {/* <Badge className='ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full'>6</Badge> */}
             </Link>
-            <Link
+            {/* <Link
               to='/dashboard/user-management'
               className={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all ${
                 location.pathname === '/dashboard/user-management'
@@ -74,7 +74,7 @@ export function SideBar() {
             >
               <img src={settingsIcon} alt='settings' className='w-6 h-6' />
               Settings
-            </Link>
+            </Link> */}
           </nav>
         </div>
       </div>
